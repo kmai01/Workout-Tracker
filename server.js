9f92155915fc7b79ba52649165ce9433d6354638
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-25 00:43:40 -0500

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan")
-
 const mongoose = require("mongoose");
+
 
 const app = express();
 
@@ -15,14 +15,13 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true,
+useUnifiedTopology: true ,
+useFindAndModify: false
+});
 
-require("./public/apiRoute.js")(app);
-require("./routes/htmlRoute.js")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT,function(){ 
     console.log(`App listening on Port ${PORT}`);
